fcbc6ad9c0927c17b63701caec432b3332d5ebd1
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-01 20:45:48 +0700

    mengubah tampilan footer

--- application/views/layout/backend/v_footer.php
+++ application/views/layout/backend/v_footer.php
@@ -29,6 +29,15 @@
 <script src="<?= base_url() ?>template/js/settings.js"></script>
 <script src="<?= base_url() ?>template/js/todolist.js"></script>
 <!-- endinject -->
+<!-- plugin js for this page -->
+<script src="<?= base_url() ?>template/vendors/typeahead.js/typeahead.bundle.min.js"></script>
+<script src="<?= base_url() ?>template/vendors/select2/select2.min.js"></script>
+<!-- End plugin js for this page -->
+<!-- Custom js for this page-->
+<script src="<?= base_url() ?>template/js/file-upload.js"></script>
+<script src="<?= base_url() ?>template/js/typeahead.js"></script>
+<script src="<?= base_url() ?>template/js/select2.js"></script>
+<!-- End custom js for this page-->
 <!-- Custom js for this page-->
 <script src="<?= base_url() ?>template/js/dashboard.js"></script>
 <!-- End custom js for this page-->
