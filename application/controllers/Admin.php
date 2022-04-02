da74ab8fa3c4f3d5ddeb2ddf945b4785cd3aefff
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-02 07:21:49 +0700

    menambahkan construck model

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -5,18 +5,18 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Admin extends CI_Controller
 {
 
-	// public function __construct()
-	// {
-	// 	parent::__construct();
-	// 	//Load Dependencies
-
-	// }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_berobat');
+	}
 
 	// List all your items
 	public function index($offset = 0)
 	{
 		$data = array(
 			'title' => 'Dashboard',
+			'grafik' => $this->m_berobat->grafik(),
 			'isi' => 'v_admin'
 		);
 		$this->load->view('layout/backend/v_wrapper', $data, FALSE);
