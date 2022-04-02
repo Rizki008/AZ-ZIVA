2c34fd62484ab32f97fcfdc194c60f56833bf824
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-02 21:49:13 +0700

    mengubah tampilan home

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -2,24 +2,24 @@
 <div class="container-fluid header bg-primary p-0 mb-5">
 	<div class="row g-0 align-items-center flex-column-reverse flex-lg-row">
 		<div class="col-lg-6 p-5 wow fadeIn" data-wow-delay="0.1s">
-			<h1 class="display-4 text-white mb-5">Good Health Is The Root Of All Heppiness</h1>
+			<h1 class="display-4 text-white mb-5">Kesehatan Yang Baik Adalah Akar Dari Semua Kebahagiaan</h1>
 			<div class="row g-4">
 				<div class="col-sm-4">
 					<div class="border-start border-light ps-4">
 						<h2 class="text-white mb-1" data-toggle="counter-up">123</h2>
-						<p class="text-light mb-0">Expert Doctors</p>
+						<p class="text-light mb-0">Dokter Ahli</p>
 					</div>
 				</div>
 				<div class="col-sm-4">
 					<div class="border-start border-light ps-4">
 						<h2 class="text-white mb-1" data-toggle="counter-up">1234</h2>
-						<p class="text-light mb-0">Medical Stuff</p>
+						<p class="text-light mb-0">Barang Medis</p>
 					</div>
 				</div>
 				<div class="col-sm-4">
 					<div class="border-start border-light ps-4">
 						<h2 class="text-white mb-1" data-toggle="counter-up">12345</h2>
-						<p class="text-light mb-0">Total Patients</p>
+						<p class="text-light mb-0">Jumlah Pasien</p>
 					</div>
 				</div>
 			</div>
@@ -62,13 +62,12 @@
 			</div>
 			<div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
 				<p class="d-inline-block border rounded-pill py-1 px-4">About Us</p>
-				<h1 class="mb-4">Why You Should Trust Us? Get Know About Us!</h1>
+				<h1 class="mb-4">Mengapa Anda Harus Mempercayai Kami? Ketahui Tentang Kami!</h1>
 				<p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
 				<p class="mb-4">Stet no et lorem dolor et diam, amet duo ut dolore vero eos. No stet est diam rebum amet diam ipsum. Clita clita labore, dolor duo nonumy clita sit at, sed sit sanctus dolor eos.</p>
-				<p><i class="far fa-check-circle text-primary me-3"></i>Quality health care</p>
-				<p><i class="far fa-check-circle text-primary me-3"></i>Only Qualified Doctors</p>
-				<p><i class="far fa-check-circle text-primary me-3"></i>Medical Research Professionals</p>
-				<a class="btn btn-primary rounded-pill py-3 px-5 mt-3" href="">Read More</a>
+				<p><i class="far fa-check-circle text-primary me-3"></i>Perawatan kesehatan yang berkualitas</p>
+				<p><i class="far fa-check-circle text-primary me-3"></i>Hanya Dokter yang Berkualifikasi</p>
+				<p><i class="far fa-check-circle text-primary me-3"></i>Profesional Riset Medis</p>
 			</div>
 		</div>
 	</div>
@@ -81,7 +80,7 @@
 	<div class="container">
 		<div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
 			<p class="d-inline-block border rounded-pill py-1 px-4">Services</p>
-			<h1>Health Care Solutions</h1>
+			<h1>Solusi Perawatan Kesehatan</h1>
 		</div>
 		<div class="row g-4">
 			<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
@@ -91,7 +90,6 @@
 					</div>
 					<h4 class="mb-3">Cardiology</h4>
 					<p class="mb-4">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.</p>
-					<a class="btn" href=""><i class="fa fa-plus text-primary me-3"></i>Read More</a>
 				</div>
 			</div>
 			<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
@@ -101,7 +99,6 @@
 					</div>
 					<h4 class="mb-3">Pulmonary</h4>
 					<p class="mb-4">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.</p>
-					<a class="btn" href=""><i class="fa fa-plus text-primary me-3"></i>Read More</a>
 				</div>
 			</div>
 			<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
@@ -111,7 +108,6 @@
 					</div>
 					<h4 class="mb-3">Neurology</h4>
 					<p class="mb-4">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.</p>
-					<a class="btn" href=""><i class="fa fa-plus text-primary me-3"></i>Read More</a>
 				</div>
 			</div>
 			<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
@@ -121,7 +117,6 @@
 					</div>
 					<h4 class="mb-3">Orthopedics</h4>
 					<p class="mb-4">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.</p>
-					<a class="btn" href=""><i class="fa fa-plus text-primary me-3"></i>Read More</a>
 				</div>
 			</div>
 			<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
@@ -131,7 +126,6 @@
 					</div>
 					<h4 class="mb-3">Dental Surgery</h4>
 					<p class="mb-4">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.</p>
-					<a class="btn" href=""><i class="fa fa-plus text-primary me-3"></i>Read More</a>
 				</div>
 			</div>
 			<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
@@ -141,7 +135,6 @@
 					</div>
 					<h4 class="mb-3">Laboratory</h4>
 					<p class="mb-4">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.</p>
-					<a class="btn" href=""><i class="fa fa-plus text-primary me-3"></i>Read More</a>
 				</div>
 			</div>
 		</div>
@@ -156,8 +149,8 @@
 		<div class="row g-0 mx-lg-0">
 			<div class="col-lg-6 feature-text py-5 wow fadeIn" data-wow-delay="0.1s">
 				<div class="p-lg-5 ps-lg-0">
-					<p class="d-inline-block border rounded-pill text-light py-1 px-4">Features</p>
-					<h1 class="text-white mb-4">Why Choose Us</h1>
+					<p class="d-inline-block border rounded-pill text-light py-1 px-4">Fitur</p>
+					<h1 class="text-white mb-4">Mengapa Memilih Kami</h1>
 					<p class="text-white mb-4 pb-2">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo erat amet</p>
 					<div class="row g-4">
 						<div class="col-6">
@@ -209,7 +202,7 @@
 			</div>
 			<div class="col-lg-6 pe-lg-0 wow fadeIn" data-wow-delay="0.5s" style="min-height: 400px;">
 				<div class="position-relative h-100">
-					<img class="position-absolute img-fluid w-100 h-100" src="img/feature.jpg" style="object-fit: cover;" alt="">
+					<img class="position-absolute img-fluid w-100 h-100" src="<?= base_url() ?>template1/img/feature.jpg" style="object-fit: cover;" alt="">
 				</div>
 			</div>
 		</div>
@@ -222,8 +215,8 @@
 <div class="container-xxl py-5">
 	<div class="container">
 		<div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-			<p class="d-inline-block border rounded-pill py-1 px-4">Doctors</p>
-			<h1>Our Experience Doctors</h1>
+			<p class="d-inline-block border rounded-pill py-1 px-4">Doktor</p>
+			<h1>Dokter Pengalaman Kami</h1>
 		</div>
 		<div class="row g-4">
 			<div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
@@ -294,115 +287,3 @@
 	</div>
 </div>
 <!-- Team End -->
-
-
-<!-- Appointment Start -->
-<div class="container-xxl py-5">
-	<div class="container">
-		<div class="row g-5">
-			<div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-				<p class="d-inline-block border rounded-pill py-1 px-4">Appointment</p>
-				<h1 class="mb-4">Make An Appointment To Visit Our Doctor</h1>
-				<p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-				<div class="bg-light rounded d-flex align-items-center p-5 mb-4">
-					<div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style="width: 55px; height: 55px;">
-						<i class="fa fa-phone-alt text-primary"></i>
-					</div>
-					<div class="ms-4">
-						<p class="mb-2">Call Us Now</p>
-						<h5 class="mb-0">+012 345 6789</h5>
-					</div>
-				</div>
-				<div class="bg-light rounded d-flex align-items-center p-5">
-					<div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style="width: 55px; height: 55px;">
-						<i class="fa fa-envelope-open text-primary"></i>
-					</div>
-					<div class="ms-4">
-						<p class="mb-2">Mail Us Now</p>
-						<h5 class="mb-0">info@example.com</h5>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
-				<div class="bg-light rounded h-100 d-flex align-items-center p-5">
-					<form>
-						<div class="row g-3">
-							<div class="col-12 col-sm-6">
-								<input type="text" class="form-control border-0" placeholder="Your Name" style="height: 55px;">
-							</div>
-							<div class="col-12 col-sm-6">
-								<input type="email" class="form-control border-0" placeholder="Your Email" style="height: 55px;">
-							</div>
-							<div class="col-12 col-sm-6">
-								<input type="text" class="form-control border-0" placeholder="Your Mobile" style="height: 55px;">
-							</div>
-							<div class="col-12 col-sm-6">
-								<select class="form-select border-0" style="height: 55px;">
-									<option selected>Choose Doctor</option>
-									<option value="1">Doctor 1</option>
-									<option value="2">Doctor 2</option>
-									<option value="3">Doctor 3</option>
-								</select>
-							</div>
-							<div class="col-12 col-sm-6">
-								<div class="date" id="date" data-target-input="nearest">
-									<input type="text" class="form-control border-0 datetimepicker-input" placeholder="Choose Date" data-target="#date" data-toggle="datetimepicker" style="height: 55px;">
-								</div>
-							</div>
-							<div class="col-12 col-sm-6">
-								<div class="time" id="time" data-target-input="nearest">
-									<input type="text" class="form-control border-0 datetimepicker-input" placeholder="Choose Date" data-target="#time" data-toggle="datetimepicker" style="height: 55px;">
-								</div>
-							</div>
-							<div class="col-12">
-								<textarea class="form-control border-0" rows="5" placeholder="Describe your problem"></textarea>
-							</div>
-							<div class="col-12">
-								<button class="btn btn-primary w-100 py-3" type="submit">Book Appointment</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<!-- Appointment End -->
-
-
-<!-- Testimonial Start -->
-<div class="container-xxl py-5">
-	<div class="container">
-		<div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-			<p class="d-inline-block border rounded-pill py-1 px-4">Testimonial</p>
-			<h1>What Say Our Patients!</h1>
-		</div>
-		<div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
-			<div class="testimonial-item text-center">
-				<img class="img-fluid bg-light rounded-circle p-2 mx-auto mb-4" src="<?= base_url() ?>template1/img/testimonial-1.jpg" style="width: 100px; height: 100px;">
-				<div class="testimonial-text rounded text-center p-4">
-					<p>Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr erat sit sit erat at rebum justo sea clita.</p>
-					<h5 class="mb-1">Patient Name</h5>
-					<span class="fst-italic">Profession</span>
-				</div>
-			</div>
-			<div class="testimonial-item text-center">
-				<img class="img-fluid bg-light rounded-circle p-2 mx-auto mb-4" src="<?= base_url() ?>template1/img/testimonial-2.jpg" style="width: 100px; height: 100px;">
-				<div class="testimonial-text rounded text-center p-4">
-					<p>Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr erat sit sit erat at rebum justo sea clita.</p>
-					<h5 class="mb-1">Patient Name</h5>
-					<span class="fst-italic">Profession</span>
-				</div>
-			</div>
-			<div class="testimonial-item text-center">
-				<img class="img-fluid bg-light rounded-circle p-2 mx-auto mb-4" src="<?= base_url() ?>template1/img/testimonial-3.jpg" style="width: 100px; height: 100px;">
-				<div class="testimonial-text rounded text-center p-4">
-					<p>Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr erat sit sit erat at rebum justo sea clita.</p>
-					<h5 class="mb-1">Patient Name</h5>
-					<span class="fst-italic">Profession</span>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<!-- Testimonial End -->
