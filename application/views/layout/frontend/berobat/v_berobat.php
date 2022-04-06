20770ae0c5ec9df94999ec05913a6a63f1943f74
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-06 15:40:38 +0700

    mengubah view front end berobat

--- application/views/layout/frontend/berobat/v_berobat.php
+++ application/views/layout/frontend/berobat/v_berobat.php
@@ -18,41 +18,19 @@
 <div class="container-xxl py-5">
 	<div class="container">
 		<div class="row g-5">
-			<div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-				<p class="d-inline-block border rounded-pill py-1 px-4">Daftar Berobat</p>
-				<h1 class="mb-4">Buat Janji untuk Mengunjungi Dokter Kami</h1>
-				<p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-				<div class="bg-light rounded d-flex align-items-center p-5 mb-4">
-					<div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style="width: 55px; height: 55px;">
-						<i class="fa fa-phone-alt text-primary"></i>
-					</div>
-					<div class="ms-4">
-						<p class="mb-2">Call Us Now</p>
-						<h5 class="mb-0">+012 345 6789</h5>
-					</div>
-				</div>
-				<div class="bg-light rounded d-flex align-items-center p-5">
-					<div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style="width: 55px; height: 55px;">
-						<i class="fa fa-envelope-open text-primary"></i>
-					</div>
-					<div class="ms-4">
-						<p class="mb-2">Mail Us Now</p>
-						<h5 class="mb-0">info@example.com</h5>
-					</div>
-				</div>
-			</div>
+
 			<div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
 				<div class="bg-light rounded h-100 d-flex align-items-center p-5">
-					<form action="<?= base_url('pasien/berobat') ?>" method="POST">
+					<form action="#" method="POST">
 						<div class="row g-3">
 							<div class="col-12 col-sm-6">
-								<input type="number" name="no_antrian" value="<?= set_value('no_antrian') ?>" class="form-control border-0" placeholder="No Antrian" style="height: 55px;">
+								<input type="number" class="form-control border-0" readonly placeholder="No Antrian" style="height: 55px;" hidden>
 							</div>
 							<div class="col-12 col-sm-6">
-								<input type="date" name="tgl_berobat" value="<?= set_value('tgl_berobat') ?>" class="form-control border-0" placeholder="Tanggal Berobat" style="height: 55px;">
+								<input type="date" class="form-control border-0" placeholder="Tanggal Berobat" style="height: 55px;" hidden>
 							</div>
 							<div class="col-12">
-								<button class="btn btn-primary w-100 py-3" type="submit">Daftar Berobat</button>
+								<a href="<?= base_url('pasien/berobat_baru') ?>" class="btn btn-primary w-100 py-3">Daftar Berobat Hari Ini</a href="<?= base_url('pasien/berobat_baru') ?>">
 							</div>
 						</div>
 					</form>
