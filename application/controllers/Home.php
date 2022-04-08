db70b95e61b22446a01d12676f90976b2cdf93e4
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-08 23:01:28 +0700

    menambahkan hapus otomatis pada no_antrian

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,11 +4,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
-
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_pasien');
+	}
 	public function index()
 	{
 		$data = array(
 			'title' => 'Home',
+			'hapus_otomatis' => $this->m_pasien->hapus_otomatis(),
+			'hapus_data' => $this->m_pasien->hapus_data(),
 			'isi' => 'v_home'
 		);
 		$this->load->view('layout/frontend/v_wrapper', $data, FALSE);
--- application/models/M_pasien.php
+++ application/models/M_pasien.php
@@ -26,4 +26,18 @@ class M_pasien extends CI_Model
 	{
 		return $this->db->query('SELECT MAX(no_antrian +1)AS antrian FROM `booking_berobat` WHERE tgl_berobat GROUP BY tgl_berobat')->row();
 	}
+
+	public function hapus_otomatis()
+	{
+		$lama =  1;
+		$query = $this->db->query("DELETE FROM booking_berobat WHERE DATEDIFF(CURDATE(), tgl_berobat) > $lama");
+		return $query;
+	}
+
+	function hapus_data()
+	{
+		$query = $this->db->query("DELETE FROM booking_berobat WHERE id_boking IN (SELECT * FROM(
+			SELECT id_boking FROM booking_berobat WHERE DATEDIFF(CURRENT_DATE, 'tgl_berobat') >= 1) AS t2)");
+		return $query;
+	}
 }
