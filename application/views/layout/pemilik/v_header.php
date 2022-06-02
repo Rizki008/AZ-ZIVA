1c26007a7e48e4eca58fbb61b675c69a6fde7414
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-02 20:56:09 +0700

    mengubah header pemilik

--- application/views/layout/pemilik/v_header.php
+++ application/views/layout/pemilik/v_header.php
@@ -1,80 +1,28 @@
 <body>
-	<div class="container-scroller">
+	<div class=" container-scroller">
 		<!-- partial:partials/_navbar.html -->
-		<nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-			<div class="navbar-brand-wrapper d-flex justify-content-center">
-				<div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
-					<a class="navbar-brand brand-logo" href="<?= base_url('pemilik') ?>"><img src="<?= base_url() ?>template/images/logo.svg" alt="logo" /></a>
-					<a class="navbar-brand brand-logo-mini" href="<?= base_url('pemilik') ?>"><img src="<?= base_url() ?>template/images/logo-mini.svg" alt="logo" /></a>
-					<button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-						<span class="typcn typcn-th-menu"></span>
-					</button>
-				</div>
+		<nav class="navbar navbar-default col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+			<div class="bg-white text-center navbar-brand-wrapper">
+				<a class="navbar-brand brand-logo" href="<?= base_url('pemilik') ?>"><img src="<?= base_url() ?>backend/images/logo_star_black.png" /></a>
+				<a class="navbar-brand brand-logo-mini" href="<?= base_url('pemilik') ?>"><img src="<?= base_url() ?>backend/images/logo_star_mini.jpg" alt=""></a>
 			</div>
-			<div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-				<ul class="navbar-nav mr-lg-2">
-					<li class="nav-item nav-profile dropdown">
-						<a class="nav-link" href="#" data-toggle="dropdown" id="profileDropdown">
-							<img src="<?= base_url() ?>template/images/faces/face5.jpg" alt="profile" />
-							<span class="nav-profile-name"><?= $this->session->userdata('username'); ?></span>
-						</a>
+			<div class="navbar-menu-wrapper d-flex align-items-center">
+				<button class="navbar-toggler navbar-toggler d-none d-lg-block navbar-dark align-self-center mr-3" type="button" data-toggle="minimize">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<form class="form-inline mt-2 mt-md-0 d-none d-lg-block">
+					<!-- <input class="form-control mr-sm-2 search" type="text" placeholder="Search"> -->
+				</form>
+				<ul class="navbar-nav ml-lg-auto d-flex align-items-center flex-row">
+					<li class="nav-item">
+						<!-- <a class="nav-link profile-pic" href="#"><img class="rounded-circle" src="<?= base_url() ?>backend/images/face.jpg" alt=""></a> -->
+					</li>
+					<li class="nav-item">
+						<!-- <a class="nav-link" href="#"><i class="fa fa-th"></i></a> -->
 					</li>
 				</ul>
-				<ul class="navbar-nav navbar-nav-right">
-					<li class="nav-item dropdown mr-0">
-						<a class="nav-link count-indicator dropdown-toggle d-flex align-items-center justify-content-center" id="notificationDropdown" href="#" data-toggle="dropdown">
-							<i class="typcn typcn-bell mx-0"></i>
-							<span class="count"></span>
-						</a>
-						<div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
-							<p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
-							<a class="dropdown-item preview-item">
-								<div class="preview-thumbnail">
-									<div class="preview-icon bg-success">
-										<i class="typcn typcn-info mx-0"></i>
-									</div>
-								</div>
-								<div class="preview-item-content">
-									<h6 class="preview-subject font-weight-normal">Application Error</h6>
-									<p class="font-weight-light small-text mb-0 text-muted">
-										Just now
-									</p>
-								</div>
-							</a>
-							<a class="dropdown-item preview-item">
-								<div class="preview-thumbnail">
-									<div class="preview-icon bg-warning">
-										<i class="typcn typcn-cog-outline mx-0"></i>
-									</div>
-								</div>
-								<div class="preview-item-content">
-									<h6 class="preview-subject font-weight-normal">Settings</h6>
-									<p class="font-weight-light small-text mb-0 text-muted">
-										Private message
-									</p>
-								</div>
-							</a>
-							<a class="dropdown-item preview-item">
-								<div class="preview-thumbnail">
-									<div class="preview-icon bg-info">
-										<i class="typcn typcn-user mx-0"></i>
-									</div>
-								</div>
-								<div class="preview-item-content">
-									<h6 class="preview-subject font-weight-normal">New user registration</h6>
-									<p class="font-weight-light small-text mb-0 text-muted">
-										2 days ago
-									</p>
-								</div>
-							</a>
-						</div>
-					</li>
-				</ul>
-				<button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-					<span class="typcn typcn-th-menu"></span>
+				<button class="navbar-toggler navbar-dark navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+					<span class="navbar-toggler-icon"></span>
 				</button>
 			</div>
 		</nav>
-		<div class="container-fluid page-body-wrapper">
-
-			<!-- partial -->
