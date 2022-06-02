df260b456be52140f2ac41949694ca9d725b0800
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-02 20:56:24 +0700

    mengubah head pemilik

--- application/views/layout/pemilik/v_head.php
+++ application/views/layout/pemilik/v_head.php
@@ -5,20 +5,12 @@
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>PolluxUI Admin</title>
-	<!-- base:css -->
-	<link rel="stylesheet" href="<?= base_url() ?>template/vendors/typicons/typicons.css">
-	<link rel="stylesheet" href="<?= base_url() ?>template/vendors/css/vendor.bundle.base.css">
-	<link rel="stylesheet" href="<?= base_url() ?>template/vendors/select2/select2.min.css">
-	<!-- endinject -->
-	<!-- plugin css for this page -->
-	<!-- End plugin css for this page -->
-	<!-- inject:css -->
-	<!-- <link rel="stylesheet" href="<?= base_url() ?>template/vendors/select2-bootstrap-theme/select2-bootstrap.min.css"> -->
-	<link rel="stylesheet" href="<?= base_url() ?>template/css/vertical-layout-light/style.css">
-	<!-- endinject -->
-	<link rel="shortcut icon" href="<?= base_url() ?>template/images/favicon.png" />
-
-	<script src="<?= base_url() ?>template/chart/dist/Chart.min.js"></script>
-	<script src="<?= base_url() ?>template/chart/samples/utils.js"></script>
+	<title><?= $title ?></title>
+	<link rel="stylesheet" href="<?= base_url() ?>backend/node_modules/font-awesome/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="<?= base_url() ?>backend/node_modules/perfect-scrollbar/dist/css/perfect-scrollbar.min.css" />
+	<link rel="stylesheet" href="<?= base_url() ?>backend/node_modules/flag-icon-css/css/flag-icon.min.css" />
+	<link rel="stylesheet" href="<?= base_url() ?>backend/css/style.css" />
+	<link rel="shortcut icon" href="<?= base_url() ?>backend/images/favicon.png" />
 </head>
+
+<body>
