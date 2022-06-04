95acbae65f6e38a0e109eab1ca9fc2064bdb1728
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-05 02:47:49 +0700

    menambahkan grafik analisi pasien

--- application/views/v_admin.php
+++ application/views/v_admin.php
@@ -1,51 +1,282 @@
 <div class="content-wrapper">
-
+	<h3 class="page-heading mb-4">Dashboard</h3>
 	<div class="row">
-		<div class="col-xl-6 grid-margin stretch-card flex-column">
-			<h5 class="mb-2 text-titlecase mb-4">Status statistics</h5>
-			<div class="row">
-				<div class="col-md-6 grid-margin stretch-card">
-					<div class="card">
-						<div class="card-body d-flex flex-column justify-content-between">
-							<div class="d-flex justify-content-between align-items-center mb-2">
-								<p class="mb-0 text-muted">Data Pasien</p>
-								<p class="mb-0 text-muted"></p>
-							</div>
-							<h4><?= $total_pasien ?></h4>
-							<canvas id="transactions-chart" class="mt-auto" height="65"></canvas>
+		<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mb-4">
+			<div class="card card-statistics">
+				<div class="card-body">
+					<div class="clearfix">
+						<div class="float-left">
+							<h4 class="text-danger">
+								<i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i>
+							</h4>
 						</div>
-					</div>
-				</div>
-				<div class="col-md-6 grid-margin stretch-card">
-					<div class="card">
-						<div class="card-body d-flex flex-column justify-content-between">
-							<div class="d-flex justify-content-between align-items-center mb-2">
-								<div>
-									<p class="mb-2 text-muted">Daftar Berobat</p>
-									<h6 class="mb-0"><?= $total_daftar ?></h6>
-								</div>
-								<div>
-									<p class="mb-2 text-muted">Total Pasien Berobat</p>
-									<h6 class="mb-0"><?= $total_berobat ?></h6>
-								</div>
-							</div>
-							<canvas id="sales-chart-a" class="mt-auto" height="65"></canvas>
+						<div class="float-right">
+							<p class="card-text text-dark">Total Pasien</p>
+							<h4 class="bold-text"><?= $total_pasien ?></h4>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="row h-100">
+		</div>
+		<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mb-4">
+			<div class="card card-statistics">
+				<div class="card-body">
+					<div class="clearfix">
+						<div class="float-left">
+							<h4 class="text-warning">
+								<i class="fa fa-shopping-cart highlight-icon" aria-hidden="true"></i>
+							</h4>
+						</div>
+						<div class="float-right">
+							<p class="card-text text-dark">Total Berobat</p>
+							<h4 class="bold-text"><?= $total_berobat ?></h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mb-4">
+			<div class="card card-statistics">
+				<div class="card-body">
+					<div class="clearfix">
+						<div class="float-left">
+							<h4 class="text-success">
+								<i class="fa fa-dollar highlight-icon" aria-hidden="true"></i>
+							</h4>
+						</div>
+						<div class="float-right">
+							<p class="card-text text-dark">Daftar Berobat</p>
+							<h4 class="bold-text"><?= $total_daftar ?></h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mb-4">
+			<div class="card card-statistics">
+				<div class="card-body">
+					<div class="clearfix">
+						<div class="float-left">
+							<h4 class="text-primary">
+								<i class="fa fa-twitter highlight-icon" aria-hidden="true"></i>
+							</h4>
+						</div>
+						<div class="float-right">
+							<p class="card-text text-dark">Total Obat</p>
+							<h4 class="bold-text"><?= $total_obat ?></h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			<div class="card card-primary">
+				<div class="card-header">
+					<h3 class="card-title">Analisis Obat Pertanggal</h3>
+				</div>
+				<div class="card-body">
+					<?php
+					echo form_open('admin/analisis_tanggal') ?>
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="form-group">
+								<label>Tanggal</label>
+								<select name="tanggal" class="form-control">
+									<?php
+									$mulai = 1;
+									for ($i = $mulai; $i < $mulai + 31; $i++) {
+										$sel = $i == date('Y') ? 'selected="selected"' : '';
+										echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+									}
+									?>
+								</select>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="form-group">
+								<label>Bulan</label>
+								<select name="bulan" class="form-control">
+									<?php
+									$mulai = 1;
+									for ($i = $mulai; $i < $mulai + 12; $i++) {
+										$sel = $i == date('Y') ? 'selected="selected"' : '';
+										echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+									}
+									?>
+								</select>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="form-group">
+								<label>Tahun</label>
+								<select name="tahun" class="form-control">
+									<?php
+									$mulai = date('Y') - 1;
+									for ($i = $mulai; $i < $mulai + 10; $i++) {
+										$sel = $i == date('Y') ? 'selected="selected"' : '';
+										echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+									}
+									?>
+								</select>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="form-group">
+								<button type="submit" class="btn btn-primary btn-block"><i class="fa fa-print"></i>Cek Hasil Analisis</button>
+							</div>
+						</div>
+					</div>
+					<?php
+					echo form_close() ?>
+				</div>
 			</div>
 		</div>
 
+		<div class="col-md-4">
+			<div class="card card-primary">
+				<div class="card-header">
+					<h3 class="card-title">Analisis Obat PerBulan</h3>
+				</div>
+				<div class="card-body">
+					<?php
+					echo form_open('admin/analisis_bulan') ?>
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="form-group">
+								<label>Bulan</label>
+								<select name="bulan" class="form-control">
+									<?php
+									$mulai = 1;
+									for ($i = $mulai; $i < $mulai + 12; $i++) {
+										$sel = $i == date('Y') ? 'selected="selected"' : '';
+										echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+									}
+									?>
+								</select>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+								<label>Tahun</label>
+								<select name="tahun" class="form-control">
+									<?php
+									$mulai = date('Y') - 1;
+									for ($i = $mulai; $i < $mulai + 10; $i++) {
+										$sel = $i == date('Y') ? 'selected="selected"' : '';
+										echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+									}
+									?>
+								</select>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="form-group">
+								<button type="submit" class="btn btn-primary btn-block"><i class="fa fa-print"></i> Cek Hasil Analisis</button>
+							</div>
+						</div>
+					</div>
+					<?php
+					echo form_close() ?>
+				</div>
+				<!-- /.card-body -->
+			</div>
+			<!-- /.card -->
+		</div>
+
+
+		<div class="col-md-4">
+			<div class="card card-primary">
+				<div class="card-header">
+					<h3 class="card-title">Analisis Obat Pertahun</h3>
+				</div>
+				<!-- /.card-header -->
+				<div class="card-body">
+					<?php
+					echo form_open('admin/analisis_tahun') ?>
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="form-group">
+								<label>Tahun</label>
+								<select name="tahun" class="form-control">
+									<?php
+									$mulai = date('Y') - 1;
+									for ($i = $mulai; $i < $mulai + 10; $i++) {
+										$sel = $i == date('Y') ? 'selected="selected"' : '';
+										echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+									}
+									?>
+								</select>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="form-group">
+								<button type="submit" class="btn btn-primary btn-block"><i class="fa fa-print"></i> Cek Hasil Analisis</button>
+							</div>
+						</div>
+					</div>
+					<?php
+					echo form_close() ?>
+				</div>
+				<!-- /.card-body -->
+			</div>
+			<!-- /.card -->
+		</div>
+	</div>
+	<br>
+	<br>
+	<div class="row">
+		<div class="col-md-6 mb-6">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">Total BPJS</h5>
+					<div class="custom-legend-container w-75 mx-auto">
+						<canvas id="myChart" height="100"></canvas>
+					</div>
+					<div id="sales-chart-legend" class="legend-right"></div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6 mb-6">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">Total Alamat</h5>
+					<div class="custom-legend-container w-75 mx-auto">
+						<canvas id="myBar" height="100"></canvas>
+					</div>
+					<div id="sales-chart-legend" class="legend-right"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br>
+	<br>
+	<div class="row">
+		<div class="col-md-6 mb-6">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">Tota Gejala</h5>
+					<div class="custom-legend-container w-75 mx-auto">
+						<canvas id="myLinda" height="100"></canvas>
+					</div>
+					<div id="sales-chart-legend" class="legend-right"></div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6 mb-6">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">Tota BPJS</h5>
+					<div class="custom-legend-container w-75 mx-auto">
+						<canvas id="myHoney" height="100"></canvas>
+					</div>
+					<div id="sales-chart-legend" class="legend-right"></div>
+				</div>
+			</div>
+		</div>
 	</div>
 
 
-
-
-	<canvas id="myChart" height="100"></canvas>
-
-
-
-
 </div>
+<!-- partial:partials/_footer.html -->
