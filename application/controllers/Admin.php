09feefa4691a4f7cf4f70a897f98d314547a6ce5
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-05 02:55:11 +0700

    menambahkan proses obat, penyakit dan alamat

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -16,6 +16,9 @@ class Admin extends CI_Controller
 	// List all your items
 	public function index($offset = 0)
 	{
+		$tanggal = $this->input->post('tanggal');
+		$bulan = $this->input->post('bulan');
+		$tahun = $this->input->post('tahun');
 		$data = array(
 			'title' => 'Dashboard',
 			'total_pasien' => $this->m_admin->total_pasien(),
@@ -24,6 +27,15 @@ class Admin extends CI_Controller
 			'total_berobat' => $this->m_admin->total_berobat(),
 			'grafik' => $this->m_berobat->grafik(),
 			'grafik_alamat' => $this->m_pasien->grafik_alamat(),
+			'grafik_alamat_pasien' => $this->m_berobat->grafik_alamat(),
+			'grafik_gejala_pasien' => $this->m_berobat->grafik_penyakit(),
+			'grafik_bpjs' => $this->m_berobat->grafik_bpjs(),
+			'tanggal' => $tanggal,
+			'bulan' => $bulan,
+			'tahun' => $tahun,
+			'analisis_tanggal' => $this->m_admin->analisis_tanggal($tanggal, $bulan, $tahun),
+			'analisis_bulan' => $this->m_admin->analisis_bulan($bulan, $tahun),
+			'analisis_tahun' => $this->m_admin->analisis_tahun($tahun),
 			'isi' => 'v_admin'
 		);
 		$this->load->view('layout/backend/v_wrapper', $data, FALSE);
@@ -52,8 +64,49 @@ class Admin extends CI_Controller
 	}
 
 	//Delete one item
-	public function delete($id = NULL)
+	public function analisis_tanggal()
 	{
+
+		$tanggal = $this->input->post('tanggal');
+		$bulan = $this->input->post('bulan');
+		$tahun = $this->input->post('tahun');
+		$data = array(
+			'title' => 'Analisis Obat Pertanggal',
+			'grafik' => $this->m_berobat->grafik(),
+			'tanggal' => $tanggal,
+			'bulan' => $bulan,
+			'tahun' => $tahun,
+			'analisis_tanggal' => $this->m_admin->analisis_tanggal($tanggal, $bulan, $tahun),
+			'isi' => 'layout/backend/analisis/v_analisis_tanggal'
+		);
+		$this->load->view('layout/backend/v_wrapper', $data, FALSE);
+	}
+	public function analisis_bulan()
+	{
+		$bulan = $this->input->post('bulan');
+		$tahun = $this->input->post('tahun');
+		$data = array(
+			'title' => 'Analisis Obat Perbulan',
+			'grafik' => $this->m_berobat->grafik(),
+			'bulan' => $bulan,
+			'tahun' => $tahun,
+			'analisis_bulan' => $this->m_admin->analisis_bulan($bulan, $tahun),
+			'isi' => 'layout/backend/analisis/v_analisis_bulan'
+		);
+		$this->load->view('layout/backend/v_wrapper', $data, FALSE);
+	}
+	public function analisis_tahun()
+	{
+
+		$tahun = $this->input->post('tahun');
+		$data = array(
+			'title' => 'Analisis Obat Pertahun',
+			'grafik' => $this->m_berobat->grafik(),
+			'tahun' => $tahun,
+			'analisis_tahun' => $this->m_admin->analisis_tahun($tahun),
+			'isi' => 'layout/backend/analisis/v_analisis_tahun'
+		);
+		$this->load->view('layout/backend/v_wrapper', $data, FALSE);
 	}
 }
 
