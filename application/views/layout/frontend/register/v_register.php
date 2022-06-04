518a437febca5c1ea33fad974b0e568d178a72cc
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-05 02:50:34 +0700

    megubah fitur pilih bpjs jadi pilihan kesehatan

--- application/views/layout/frontend/register/v_register.php
+++ application/views/layout/frontend/register/v_register.php
@@ -4,7 +4,7 @@
 		<h1 class="display-3 text-white mb-3 animated slideInDown">Appointment</h1>
 		<nav aria-label="breadcrumb animated slideInDown">
 			<ol class="breadcrumb text-uppercase mb-0">
-				<li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
+				<li class="breadcrumb-item"><a class="text-white" href="<?= base_url() ?>">Home</a></li>
 				<li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
 				<li class="breadcrumb-item text-primary active" aria-current="page">Appointment</li>
 			</ol>
@@ -71,7 +71,7 @@
 						</div>
 						<div class="col-12 col-sm-6">
 							<select class="form-select border-0" style="height: 55px;" name="bpjs">
-								<option selected>---BPJS---</option>
+								<option selected>---Pilih Penggunaan Kartu Kesehatan---</option>
 								<option value="1">BPJS</option>
 								<option value="2">Non BPJS</option>
 							</select>
