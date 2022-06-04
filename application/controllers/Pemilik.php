c15293656e4f483b01fa78afc652c25ebcd10412
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-05 02:53:13 +0700

    menambahkan proses analisis grafik

--- application/controllers/Pemilik.php
+++ application/controllers/Pemilik.php
@@ -16,6 +16,9 @@ class Pemilik extends CI_Controller
 	// List all your items
 	public function index($offset = 0)
 	{
+		$tanggal = $this->input->post('tanggal');
+		$bulan = $this->input->post('bulan');
+		$tahun = $this->input->post('tahun');
 		$data = array(
 			'title' => 'Dashboard',
 			'total_pasien' => $this->m_admin->total_pasien(),
@@ -24,34 +27,66 @@ class Pemilik extends CI_Controller
 			'total_berobat' => $this->m_admin->total_berobat(),
 			'grafik' => $this->m_berobat->grafik(),
 			'grafik_alamat' => $this->m_pasien->grafik_alamat(),
+			'grafik_alamat_pasien' => $this->m_berobat->grafik_alamat(),
+			'grafik_gejala_pasien' => $this->m_berobat->grafik_penyakit(),
+			'grafik_nonbpjs' => $this->m_berobat->grafik_nonbpjs(),
+			'grafik_bpjs_analis' => $this->m_berobat->grafik_bpjs_analis(),
+			'tanggal' => $tanggal,
+			'bulan' => $bulan,
+			'tahun' => $tahun,
+			'analisis_tanggal' => $this->m_admin->analisis_tanggal($tanggal, $bulan, $tahun),
+			'analisis_bulan' => $this->m_admin->analisis_bulan($bulan, $tahun),
+			'analisis_tahun' => $this->m_admin->analisis_tahun($tahun),
 			'isi' => 'v_pemilik'
 		);
 		$this->load->view('layout/pemilik/v_wrapper', $data, FALSE);
 	}
 
-	
 
-	// // Add a new item
-	// public function bpjs()
-	// {
-	// 	$data = array(
-	// 		'title' => 'Data Pasien BPJS',
-	// 		'bpjs' => $this->m_admin->bpjs(),
-	// 		'isi' => 'layout/backend/pasien/v_bpjs'
-	// 	);
-	// 	$this->load->view('layout/backend/v_wrapper', $data, FALSE);
-	// }
+	public function analisis_tanggal()
+	{
 
-	// //Update one item
-	// public function non_bpjs()
-	// {
-	// 	$data = array(
-	// 		'title' => 'Data Pasien BPJS',
-	// 		'non_bpjs' => $this->m_admin->non_bpjs(),
-	// 		'isi' => 'layout/backend/pasien/v_non_bpjs'
-	// 	);
-	// 	$this->load->view('layout/backend/v_wrapper', $data, FALSE);
-	// }
+		$tanggal = $this->input->post('tanggal');
+		$bulan = $this->input->post('bulan');
+		$tahun = $this->input->post('tahun');
+		$data = array(
+			'title' => 'Analisis Obat Pertanggal',
+			'grafik' => $this->m_berobat->grafik(),
+			'tanggal' => $tanggal,
+			'bulan' => $bulan,
+			'tahun' => $tahun,
+			'analisis_tanggal' => $this->m_admin->analisis_tanggal($tanggal, $bulan, $tahun),
+			'isi' => 'layout/pemilik/analisis/v_analisis_tanggal'
+		);
+		$this->load->view('layout/pemilik/v_wrapper', $data, FALSE);
+	}
+	public function analisis_bulan()
+	{
+		$bulan = $this->input->post('bulan');
+		$tahun = $this->input->post('tahun');
+		$data = array(
+			'title' => 'Analisis Obat Perbulan',
+			'grafik' => $this->m_berobat->grafik(),
+			'bulan' => $bulan,
+			'tahun' => $tahun,
+			'analisis_bulan' => $this->m_admin->analisis_bulan($bulan, $tahun),
+			'isi' => 'layout/pemilik/analisis/v_analisis_bulan'
+		);
+		$this->load->view('layout/pemilik/v_wrapper', $data, FALSE);
+	}
+	public function analisis_tahun()
+	{
+
+		$tahun = $this->input->post('tahun');
+		$data = array(
+			'title' => 'Analisis Obat Pertahun',
+			'grafik' => $this->m_berobat->grafik(),
+			'tahun' => $tahun,
+			'analisis_tahun' => $this->m_admin->analisis_tahun($tahun),
+			'isi' => 'layout/pemilik/analisis/v_analisis_tahun'
+		);
+		$this->load->view('layout/pemilik/v_wrapper', $data, FALSE);
+	}
 }
 
 /* End of file Home.php */
