714e28614546139e10742277becfefcbed99cb8e
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-06 06:21:15 +0700

    mengubah warna pada navbar dan header

--- application/views/layout/apotek/v_header.php
+++ application/views/layout/apotek/v_header.php
@@ -1,7 +1,7 @@
 <body class="hold-transition sidebar-mini layout-fixed">
 	<div class="wrapper">
 		<!-- Navbar -->
-		<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+		<nav class="main-header navbar navbar-expand navbar-dark navbar-success">
 			<!-- Left navbar links -->
 			<ul class="navbar-nav">
 				<li class="nav-item">
--- application/views/layout/backend/v_nav.php
+++ application/views/layout/backend/v_nav.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar elevation-4 sidebar-light-lime">
 	<!-- Brand Logo -->
-	<a href="index3.html" class="brand-link">
+	<a href="index3.html" class="brand-link navbar-success">
 		<img src="<?= base_url() ?>template/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
 		<span class="brand-text font-weight-light">Az-Ziva</span>
 	</a>
