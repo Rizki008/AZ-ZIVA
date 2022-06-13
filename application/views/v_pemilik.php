21653b77f5a558261e0d1a156bdb3f5b54300e02
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-13 08:11:01 +0700

    menambahkan fitur grafik analisis stock obat dan obat keluar

--- application/views/v_pemilik.php
+++ application/views/v_pemilik.php
@@ -86,160 +86,6 @@
 			</div>
 			<!-- /.row -->
 			<!-- Main row -->
-			<div class="row">
-				<div class="col-md-4">
-					<div class="card card-primary">
-						<div class="card-header">
-							<h3 class="card-title">Analisis Obat Pertanggal</h3>
-						</div>
-						<div class="card-body">
-							<?php
-							echo form_open('pemilik/analisis_tanggal') ?>
-							<div class="row">
-								<div class="col-sm-4">
-									<div class="form-group">
-										<label>Tanggal</label>
-										<select name="tanggal" class="form-control">
-											<?php
-											$mulai = 1;
-											for ($i = $mulai; $i < $mulai + 31; $i++) {
-												$sel = $i == date('Y') ? 'selected="selected"' : '';
-												echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
-											}
-											?>
-										</select>
-									</div>
-								</div>
-								<div class="col-sm-4">
-									<div class="form-group">
-										<label>Bulan</label>
-										<select name="bulan" class="form-control">
-											<?php
-											$mulai = 1;
-											for ($i = $mulai; $i < $mulai + 12; $i++) {
-												$sel = $i == date('Y') ? 'selected="selected"' : '';
-												echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
-											}
-											?>
-										</select>
-									</div>
-								</div>
-								<div class="col-sm-4">
-									<div class="form-group">
-										<label>Tahun</label>
-										<select name="tahun" class="form-control">
-											<?php
-											$mulai = date('Y') - 1;
-											for ($i = $mulai; $i < $mulai + 10; $i++) {
-												$sel = $i == date('Y') ? 'selected="selected"' : '';
-												echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
-											}
-											?>
-										</select>
-									</div>
-								</div>
-								<div class="col-sm-12">
-									<div class="form-group">
-										<button type="submit" class="btn btn-primary btn-block"><i class="fa fa-print"></i>Cek Hasil Analisis</button>
-									</div>
-								</div>
-							</div>
-							<?php
-							echo form_close() ?>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-4">
-					<div class="card card-primary">
-						<div class="card-header">
-							<h3 class="card-title">Analisis Obat PerBulan</h3>
-						</div>
-						<div class="card-body">
-							<?php
-							echo form_open('pemilik/analisis_bulan') ?>
-							<div class="row">
-								<div class="col-sm-6">
-									<div class="form-group">
-										<label>Bulan</label>
-										<select name="bulan" class="form-control">
-											<?php
-											$mulai = 1;
-											for ($i = $mulai; $i < $mulai + 12; $i++) {
-												$sel = $i == date('Y') ? 'selected="selected"' : '';
-												echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
-											}
-											?>
-										</select>
-									</div>
-								</div>
-								<div class="col-sm-6">
-									<div class="form-group">
-										<label>Tahun</label>
-										<select name="tahun" class="form-control">
-											<?php
-											$mulai = date('Y') - 1;
-											for ($i = $mulai; $i < $mulai + 10; $i++) {
-												$sel = $i == date('Y') ? 'selected="selected"' : '';
-												echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
-											}
-											?>
-										</select>
-									</div>
-								</div>
-								<div class="col-sm-12">
-									<div class="form-group">
-										<button type="submit" class="btn btn-primary btn-block"><i class="fa fa-print"></i> Cek Hasil Analisis</button>
-									</div>
-								</div>
-							</div>
-							<?php
-							echo form_close() ?>
-						</div>
-						<!-- /.card-body -->
-					</div>
-					<!-- /.card -->
-				</div>
-
-
-				<div class="col-md-4">
-					<div class="card card-primary">
-						<div class="card-header">
-							<h3 class="card-title">Analisis Obat Pertahun</h3>
-						</div>
-						<!-- /.card-header -->
-						<div class="card-body">
-							<?php
-							echo form_open('pemilik/analisis_tahun') ?>
-							<div class="row">
-								<div class="col-sm-12">
-									<div class="form-group">
-										<label>Tahun</label>
-										<select name="tahun" class="form-control">
-											<?php
-											$mulai = date('Y') - 1;
-											for ($i = $mulai; $i < $mulai + 10; $i++) {
-												$sel = $i == date('Y') ? 'selected="selected"' : '';
-												echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
-											}
-											?>
-										</select>
-									</div>
-								</div>
-								<div class="col-sm-12">
-									<div class="form-group">
-										<button type="submit" class="btn btn-primary btn-block"><i class="fa fa-print"></i> Cek Hasil Analisis</button>
-									</div>
-								</div>
-							</div>
-							<?php
-							echo form_close() ?>
-						</div>
-						<!-- /.card-body -->
-					</div>
-					<!-- /.card -->
-				</div>
-			</div>
 		</div>
 		<div class="row">
 			<!-- Left col -->
@@ -625,6 +471,255 @@
 						<!-- /.row -->
 					</div>
 					<!-- /.card-footer -->
+				</div>
+				<!-- /.card -->
+			</section>
+			<section class="col-lg-7 connectedSortable">
+				<!-- Custom tabs (Charts with tabs)-->
+				<div class="card">
+					<div class="card-header">
+						<h3 class="card-title">
+							<i class="fas fa-chart-pie mr-1"></i>
+							Grafik Analisis Data Obat
+						</h3>
+						<div class="card-tools">
+							<ul class="nav nav-pills ml-auto">
+								<li class="nav-item">
+									<a class="nav-link active" href="#stock-chart" data-toggle="tab">Analisis Stock Obat</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#obat-chart" data-toggle="tab">Analisis Obat Digunakan</a>
+								</li>
+							</ul>
+						</div>
+					</div><!-- /.card-header -->
+					<div class="card-body">
+						<div class="tab-content p-0">
+							<!-- Morris chart - obat -->
+							<div class="chart tab-pane active" id="stock-chart" style="position: relative; height: 300px;">
+								<!-- analisis gejala pasien  -->
+								<?php
+								foreach ($grafik_stock_obat as $key => $value) {
+									$nama_obat[] = $value->nama_obat;
+									$total_stock[] = $value->total_stock;
+								}
+								?>
+								<!-- grafik analisis grjala -->
+								<canvas id="stock" height="100" style="height: 100px;"></canvas>
+								<script>
+									var ctx = document.getElementById('stock');
+									var stock = new Chart(ctx, {
+										type: 'bar',
+										data: {
+											labels: <?= json_encode($nama_obat) ?>,
+											datasets: [{
+												label: 'Grafik Analisis stock obat',
+												data: <?= json_encode($total_stock) ?>,
+												backgroundColor: [
+													'rgba(255, 99, 132, 0.80)',
+													'rgba(54, 162, 235, 0.80)',
+													'rgba(255, 206, 86, 0.80)',
+													'rgba(75, 192, 192, 0.80)',
+													'rgba(153, 102, 255, 0.80)',
+													'rgba(255, 159, 64, 0.80)',
+													'rgba(201, 76, 76, 0.3)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(0, 140, 162, 1)',
+													'rgba(158, 109, 8, 1)',
+													'rgba(201, 76, 76, 0.8)',
+													'rgba(0, 129, 212, 1)',
+													'rgba(201, 77, 201, 1)',
+													'rgba(255, 207, 207, 1)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(128, 98, 98, 1)',
+													'rgba(0, 0, 0, 1)',
+													'rgba(128, 128, 128, 1)',
+													'rgba(255, 99, 132, 0.80)',
+													'rgba(54, 162, 235, 0.80)',
+													'rgba(255, 206, 86, 0.80)',
+													'rgba(75, 192, 192, 0.80)',
+													'rgba(153, 102, 255, 0.80)',
+													'rgba(255, 159, 64, 0.80)',
+													'rgba(201, 76, 76, 0.3)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(0, 140, 162, 1)',
+													'rgba(158, 109, 8, 1)',
+													'rgba(201, 76, 76, 0.8)',
+													'rgba(0, 129, 212, 1)',
+													'rgba(201, 77, 201, 1)',
+													'rgba(255, 207, 207, 1)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(128, 98, 98, 1)',
+													'rgba(0, 0, 0, 1)',
+													'rgba(128, 128, 128, 1)'
+												],
+												borderColor: [
+													'rgba(255, 99, 132, 1)',
+													'rgba(54, 162, 235, 1)',
+													'rgba(255, 206, 86, 1)',
+													'rgba(75, 192, 192, 1)',
+													'rgba(153, 102, 255, 1)',
+													'rgba(255, 159, 64, 1)',
+													'rgba(201, 76, 76, 0.3)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(0, 140, 162, 1)',
+													'rgba(158, 109, 8, 1)',
+													'rgba(201, 76, 76, 0.8)',
+													'rgba(0, 129, 212, 1)',
+													'rgba(201, 77, 201, 1)',
+													'rgba(255, 207, 207, 1)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(128, 98, 98, 1)',
+													'rgba(0, 0, 0, 1)',
+													'rgba(128, 128, 128, 1)',
+													'rgba(255, 99, 132, 1)',
+													'rgba(54, 162, 235, 1)',
+													'rgba(255, 206, 86, 1)',
+													'rgba(75, 192, 192, 1)',
+													'rgba(153, 102, 255, 1)',
+													'rgba(255, 159, 64, 1)',
+													'rgba(201, 76, 76, 0.3)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(0, 140, 162, 1)',
+													'rgba(158, 109, 8, 1)',
+													'rgba(201, 76, 76, 0.8)',
+													'rgba(0, 129, 212, 1)',
+													'rgba(201, 77, 201, 1)',
+													'rgba(255, 207, 207, 1)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(128, 98, 98, 1)',
+													'rgba(0, 0, 0, 1)',
+													'rgba(128, 128, 128, 1)'
+												],
+												fill: false,
+												borderWidth: 1
+											}]
+										},
+										options: {
+											scales: {
+												yAxes: [{
+													ticks: {
+														beginAtZero: true
+													}
+												}]
+											}
+										}
+									});
+								</script>
+							</div>
+							<div class="chart tab-pane" id="obat-chart" style="position: relative; height: 300px;">
+								<!-- analisis gejala pasien  -->
+								<?php
+								foreach ($obat_keluar as $key => $value) {
+									$nama_obat[] = $value->nama_obat;
+									$total_qty[] = $value->total_qty;
+								}
+								?>
+								<!-- grafik analisis grjala -->
+								<canvas id="obat" height="100" style="height: 100px;"></canvas>
+								<script>
+									var ctx = document.getElementById('obat');
+									var obat = new Chart(ctx, {
+										type: 'bar',
+										data: {
+											labels: <?= json_encode($nama_obat) ?>,
+											datasets: [{
+												label: 'Grafik Analisis Obat yang digunakan',
+												data: <?= json_encode($total_qty) ?>,
+												backgroundColor: [
+													'rgba(255, 99, 132, 0.80)',
+													'rgba(54, 162, 235, 0.80)',
+													'rgba(255, 206, 86, 0.80)',
+													'rgba(75, 192, 192, 0.80)',
+													'rgba(153, 102, 255, 0.80)',
+													'rgba(255, 159, 64, 0.80)',
+													'rgba(201, 76, 76, 0.3)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(0, 140, 162, 1)',
+													'rgba(158, 109, 8, 1)',
+													'rgba(201, 76, 76, 0.8)',
+													'rgba(0, 129, 212, 1)',
+													'rgba(201, 77, 201, 1)',
+													'rgba(255, 207, 207, 1)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(128, 98, 98, 1)',
+													'rgba(0, 0, 0, 1)',
+													'rgba(128, 128, 128, 1)',
+													'rgba(255, 99, 132, 0.80)',
+													'rgba(54, 162, 235, 0.80)',
+													'rgba(255, 206, 86, 0.80)',
+													'rgba(75, 192, 192, 0.80)',
+													'rgba(153, 102, 255, 0.80)',
+													'rgba(255, 159, 64, 0.80)',
+													'rgba(201, 76, 76, 0.3)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(0, 140, 162, 1)',
+													'rgba(158, 109, 8, 1)',
+													'rgba(201, 76, 76, 0.8)',
+													'rgba(0, 129, 212, 1)',
+													'rgba(201, 77, 201, 1)',
+													'rgba(255, 207, 207, 1)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(128, 98, 98, 1)',
+													'rgba(0, 0, 0, 1)',
+													'rgba(128, 128, 128, 1)'
+												],
+												borderColor: [
+													'rgba(255, 99, 132, 1)',
+													'rgba(54, 162, 235, 1)',
+													'rgba(255, 206, 86, 1)',
+													'rgba(75, 192, 192, 1)',
+													'rgba(153, 102, 255, 1)',
+													'rgba(255, 159, 64, 1)',
+													'rgba(201, 76, 76, 0.3)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(0, 140, 162, 1)',
+													'rgba(158, 109, 8, 1)',
+													'rgba(201, 76, 76, 0.8)',
+													'rgba(0, 129, 212, 1)',
+													'rgba(201, 77, 201, 1)',
+													'rgba(255, 207, 207, 1)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(128, 98, 98, 1)',
+													'rgba(0, 0, 0, 1)',
+													'rgba(128, 128, 128, 1)',
+													'rgba(255, 99, 132, 1)',
+													'rgba(54, 162, 235, 1)',
+													'rgba(255, 206, 86, 1)',
+													'rgba(75, 192, 192, 1)',
+													'rgba(153, 102, 255, 1)',
+													'rgba(255, 159, 64, 1)',
+													'rgba(201, 76, 76, 0.3)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(0, 140, 162, 1)',
+													'rgba(158, 109, 8, 1)',
+													'rgba(201, 76, 76, 0.8)',
+													'rgba(0, 129, 212, 1)',
+													'rgba(201, 77, 201, 1)',
+													'rgba(255, 207, 207, 1)',
+													'rgba(201, 77, 77, 1)',
+													'rgba(128, 98, 98, 1)',
+													'rgba(0, 0, 0, 1)',
+													'rgba(128, 128, 128, 1)'
+												],
+												fill: false,
+												borderWidth: 1
+											}]
+										},
+										options: {
+											scales: {
+												yAxes: [{
+													ticks: {
+														beginAtZero: true
+													}
+												}]
+											}
+										}
+									});
+								</script>
+							</div>
+						</div>
+					</div><!-- /.card-body -->
 				</div>
 				<!-- /.card -->
 			</section>
