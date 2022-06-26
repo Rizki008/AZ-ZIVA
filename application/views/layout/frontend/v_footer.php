a3ac5aaa83c40fa62e012291b0ad2391eb816c91
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-26 10:23:07 +0700

    menghapus fitur tidak penting

--- application/views/layout/frontend/v_footer.php
+++ application/views/layout/frontend/v_footer.php
@@ -4,39 +4,8 @@
 		<div class="row g-5">
 			<div class="col-lg-3 col-md-6">
 				<h5 class="text-light mb-4">Address</h5>
-				<p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Kuningan</p>
-				<p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+62 345 67890</p>
-				<p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
-				<div class="d-flex pt-2">
-					<a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-twitter"></i></a>
-					<a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>
-					<a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-youtube"></i></a>
-					<a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-linkedin-in"></i></a>
-				</div>
-			</div>
-			<div class="col-lg-3 col-md-6">
-				<h5 class="text-light mb-4">Services</h5>
-				<a class="btn btn-link" href="">Cardiology</a>
-				<a class="btn btn-link" href="">Pulmonary</a>
-				<a class="btn btn-link" href="">Neurology</a>
-				<a class="btn btn-link" href="">Orthopedics</a>
-				<a class="btn btn-link" href="">Laboratory</a>
-			</div>
-			<div class="col-lg-3 col-md-6">
-				<h5 class="text-light mb-4">Quick Links</h5>
-				<a class="btn btn-link" href="">About Us</a>
-				<a class="btn btn-link" href="">Contact Us</a>
-				<a class="btn btn-link" href="">Our Services</a>
-				<a class="btn btn-link" href="">Terms & Condition</a>
-				<a class="btn btn-link" href="">Support</a>
-			</div>
-			<div class="col-lg-3 col-md-6">
-				<h5 class="text-light mb-4">Newsletter</h5>
-				<p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-				<div class="position-relative mx-auto" style="max-width: 400px;">
-					<input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-					<button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-				</div>
+				<p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>wilayah Desa Kedungarum tepatnya di Jl. Moch Yamin no 28, Kedungarum, Kuningan.</p>
+				<p class="mb-2"><i class="fa fa-phone-alt me-3"></i>023 2889 2653</p>
 			</div>
 		</div>
 	</div>
@@ -44,7 +13,7 @@
 		<div class="copyright">
 			<div class="row">
 				<div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-					&copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
+					&copy; <a class="border-bottom" href="#">Az-Ziva</a>, All Right Reserved.
 				</div>
 			</div>
 		</div>
