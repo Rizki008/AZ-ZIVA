e56238c9af253943c11e27d06e60e24f4c9b6ea9
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-28 05:05:06 +0700

    menghapus informasi tidak penting

--- application/views/layout/frontend/login/v_login.php
+++ application/views/layout/frontend/login/v_login.php
@@ -19,25 +19,14 @@
 	<div class="container">
 		<div class="row g-5">
 			<div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-				<p class="d-inline-block border rounded-pill py-1 px-4">Appointment</p>
-				<h1 class="mb-4">Make An Appointment To Visit Our Doctor</h1>
-				<p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
+				<p class="d-inline-block border rounded-pill py-1 px-4">Az-Ziva</p>
 				<div class="bg-light rounded d-flex align-items-center p-5 mb-4">
 					<div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style="width: 55px; height: 55px;">
 						<i class="fa fa-phone-alt text-primary"></i>
 					</div>
 					<div class="ms-4">
-						<p class="mb-2">Call Us Now</p>
-						<h5 class="mb-0">+012 345 6789</h5>
-					</div>
-				</div>
-				<div class="bg-light rounded d-flex align-items-center p-5">
-					<div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style="width: 55px; height: 55px;">
-						<i class="fa fa-envelope-open text-primary"></i>
-					</div>
-					<div class="ms-4">
-						<p class="mb-2">Mail Us Now</p>
-						<h5 class="mb-0">info@example.com</h5>
+						<p class="mb-2">Kontak Darurat</p>
+						<h5 class="mb-0">023 2889 2653</h5>
 					</div>
 				</div>
 			</div>
