267dbd0c4307f64f5503f84bb1b28ab32bda3340
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-28 05:04:50 +0700

    menghapus informasi tidak penting

--- application/views/layout/frontend/register/v_register.php
+++ application/views/layout/frontend/register/v_register.php
@@ -20,15 +20,13 @@
 		<div class="row g-5">
 			<div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
 				<p class="d-inline-block border rounded-pill py-1 px-4">Klinik Az-Ziva</p>
-				<h1 class="mb-4">Tentang Kami</h1>
-				<p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
 				<div class="bg-light rounded d-flex align-items-center p-5 mb-4">
 					<div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style="width: 55px; height: 55px;">
 						<i class="fa fa-phone-alt text-primary"></i>
 					</div>
 					<div class="ms-4">
 						<p class="mb-2">Kontak Darurat</p>
-						<h5 class="mb-0">+012 345 6789</h5>
+						<h5 class="mb-0">023 2889 2653</h5>
 					</div>
 				</div>
 			</div>
